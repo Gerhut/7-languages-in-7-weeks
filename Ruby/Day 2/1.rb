9841c52ce5b41feabb782aff2f4cfe699f793ee3
iMac1 <iMac1@lab>
2013-09-09 15:27:29 +0800

    Use print instead of puts to clearify loops

--- Ruby/Day 2/1.rb
+++ Ruby/Day 2/1.rb
@@ -1,7 +1,10 @@
 require 'enumerator'
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-a[0...4].each {|i| puts i}
-a[4...8].each {|i| puts i}
-a[8...12].each {|i| puts i}
-a[12...16].each {|i| puts i}
-a.each_slice(4) {|i| puts i}
+a = (1..16).to_a
+(0..12).step(4) do |i|
+  a[i...i + 4].each {|i| print i}
+  puts
+end
+a.each_slice(4) do |i|
+  print i
+  puts
+end

91b1d9e315d39c93e8eb3c252934b488784016b5
iMac1 <iMac1@lab>
2013-09-09 15:19:00 +0800

    Use begin-end statement to avoid setting initial value of i, compare only once when i == n

--- Ruby/Day 1/6.rb
+++ Ruby/Day 1/6.rb
@@ -1,9 +1,8 @@
 n = rand(10)
-i = -1
-until i == n
-  puts "Guess: "
+begin
+  print "Guess: "
   i = gets.to_i
   puts "Higher." if i > n
   puts "Lower." if i < n
-  puts "Right." if i == n
-end
+end until i == n
+puts "Right."

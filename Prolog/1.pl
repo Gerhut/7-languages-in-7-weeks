a4a2cc69419c2362e454450410d9a9ad3512d41b
iMac1 <iMac1@lab>
2013-09-11 20:31:05 +0800

    Update Prolog Day 1

---
+++ Prolog/1.pl
@@ -0,0 +1,4 @@
+author(book1, author1).
+author(book2, author2).
+author(book3, author2).
+author(book4, author1).
---
+++ Prolog/3.pl
@@ -0,0 +1,7 @@
+use_instrument(musician1, guitar).
+use_instrument(musician2, piano).
+use_instrument(musician3, guitar).
+
+music_style(musician1, rock).
+music_style(musician2, pop).
+music_style(musician3, classical).
